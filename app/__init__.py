31bc26af42bbaff73cc53c8d3694bb5532f1ee5e
Rick Prins <r.prins@mulierinstituut.nl>
2019-11-07 22:57:59 +0100

    Removed flask share

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
-from flask_share import Share
 
 # first create extension instances, which will be initialised in create_app
 db = SQLAlchemy()
@@ -15,7 +14,6 @@ migrate = Migrate()
 login = LoginManager()
 login.login_view = "auth.login"
 bootstrap = Bootstrap()
-share = Share()
 
 
 def create_app(config_class=Config):
@@ -27,7 +25,6 @@ def create_app(config_class=Config):
     migrate.init_app(app, db)
     login.init_app(app)
     bootstrap.init_app(app)
-    share.init_app(app)
 
     # register the blueprints
     from app.auth import bp as auth_bp
